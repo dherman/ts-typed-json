a2dfd16fbca74ecf14c0eee1b83116b2e09bf32d
Dave Herman <dherman@mozilla.com>
2017-04-28 00:45:43 -0700

    drop the `Json`- prefix from type names

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,32 @@
 import Dict from 'ts-dict';
 
+export { Object_ as Object, Array_ as Array };
+
 /** JSON data, as returned by `JSON.parse()`. */
-export type JsonValue = null | boolean | number | string | JsonObject | JsonArray;
+export type Value = null | boolean | number | string | Object_ | Array_;
 
 /** JSON object values. */
-export interface JsonObject extends Dict<JsonValue> {}
+interface Object_ extends Dict<Value> {}
 
 /** JSON array values. */
-export interface JsonArray extends Array<JsonValue> {}
+interface Array_ extends Array<Value> {}
 
 /** Tests a JSON value to see if it is a JSON object. */
-export function isObject(x: JsonValue): x is JsonObject {
+export function isObject(x: Value): x is Object_ {
     return !!x && typeof x === 'object' && !Array.isArray(x);
 }
 
 /** Tests a JSON value to see if it is a JSON array. */
-export function isArray(x: JsonValue): x is JsonArray {
+export function isArray(x: Value): x is Array_ {
     return Array.isArray(x);
 }
 
 /** A more safely typed version of `JSON.parse()`. */
-export function parse(source: string): JsonValue {
+export function parse(source: string): Value {
     return JSON.parse(source);
 }
 
 /** A more safely typed version of `JSON.stringify()`. */
-export function stringify(value: JsonValue): string {
+export function stringify(value: Value): string {
     return JSON.stringify(value);
 }
